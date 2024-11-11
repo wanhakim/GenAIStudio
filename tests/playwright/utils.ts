ba4aa1bb095683c888b1cb5f6a40ad225260a4c9
wwanarif <wan.abdul.hakim.bin.wan.arif@intel.com>
2024-11-11 16:29:41 +0000

    update

--- tests/playwright/utils.ts
+++ tests/playwright/utils.ts
@@ -3,6 +3,10 @@ import { expect } from '@playwright/test';
 export async function waitForStatusText(page: any, selector: string, statusText: string, maxAttempts: number = 5, intervalTimeout: number = 60000) {
     for (let i = 0; i < maxAttempts; i++) {
         try {
+            const text = await page.locator(selector).first().innerText();
+            if (text === 'Error') {
+                throw new Error(`Encountered unwanted status text "Error" in element "${selector}"`);
+            }
             await expect(page.locator(selector).first()).toHaveText(statusText, { timeout: intervalTimeout });
             return;
         } catch (error) {
